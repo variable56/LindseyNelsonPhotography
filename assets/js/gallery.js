23e23196c654c4f079b27ebd7310f4c77a91a148
variable56 <variable56@gmail.com>
2019-01-03 21:09:59 -0700

    uploaded school project

---
+++ assets/js/gallery.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function(){
+  $('.multiple-items').slick();
+});
+
+// $('.multiple-items').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 3
+// });
